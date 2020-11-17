fb34bc9090f217d87fad07ab545779c503616370
Neill Bogie <neillbogie@googlemail.com>
2020-11-17 22:58:19 +0000

    add a few unused shader uniforms

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,4 +1,8 @@
 uniform float time;
+uniform float progress;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform vec4 resolution;
 
 varying vec2 vUv;
 varying vec4 vPosition;
@@ -6,5 +10,5 @@ varying vec4 vPosition;
 void main(){
 	
 	vec3 color1=vec3(fract(time/5.),.847,.745);
-	gl_FragColor=vec4(color1,1.);
+	gl_FragColor=vec4(vUv,0.,1.);
 }
