eab5562f26b646bcd301b4a3e08aed06090b3b9b
Neill Bogie <neillbogie@googlemail.com>
2020-11-18 00:40:32 +0000

    formatting only (non-compact formatter)

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -14,123 +14,123 @@ varying vec3 vPosition;
 //	Classic Perlin 3D Noise
 //	by Stefan Gustavson
 //
-vec4 permute(vec4 x){return mod(((x*34.)+1.)*x,289.);}
-vec4 taylorInvSqrt(vec4 r){return 1.79284291400159-.85373472095314*r;}
-vec3 fade(vec3 t){return t*t*t*(t*(t*6.-15.)+10.);}
+vec4 permute(vec4 x) {return mod(((x * 34.0) + 1.0) * x, 289.0); }
+vec4 taylorInvSqrt(vec4 r) {return 1.79284291400159 - 0.85373472095314 * r; }
+vec3 fade(vec3 t) {return t * t*t * (t * (t * 6.0 - 15.0) + 10.0); }
 
-float cnoise(vec3 P){
-	vec3 Pi0=floor(P);// Integer part for indexing
-	vec3 Pi1=Pi0+vec3(1.);// Integer part + 1
-	Pi0=mod(Pi0,289.);
-	Pi1=mod(Pi1,289.);
-	vec3 Pf0=fract(P);// Fractional part for interpolation
-	vec3 Pf1=Pf0-vec3(1.);// Fractional part - 1.0
-	vec4 ix=vec4(Pi0.x,Pi1.x,Pi0.x,Pi1.x);
-	vec4 iy=vec4(Pi0.yy,Pi1.yy);
-	vec4 iz0=Pi0.zzzz;
-	vec4 iz1=Pi1.zzzz;
+float cnoise(vec3 P) {
+	vec3 Pi0 = floor(P); // Integer part for indexing
+	vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+	Pi0 = mod(Pi0, 289.0);
+	Pi1 = mod(Pi1, 289.0);
+	vec3 Pf0 = fract(P); // Fractional part for interpolation
+	vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+	vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+	vec4 iy = vec4(Pi0.yy, Pi1.yy);
+	vec4 iz0 = Pi0.zzzz;
+	vec4 iz1 = Pi1.zzzz;
 	
-	vec4 ixy=permute(permute(ix)+iy);
-	vec4 ixy0=permute(ixy+iz0);
-	vec4 ixy1=permute(ixy+iz1);
+	vec4 ixy = permute(permute(ix) + iy);
+	vec4 ixy0 = permute(ixy + iz0);
+	vec4 ixy1 = permute(ixy + iz1);
 	
-	vec4 gx0=ixy0/7.;
-	vec4 gy0=fract(floor(gx0)/7.)-.5;
-	gx0=fract(gx0);
-	vec4 gz0=vec4(.5)-abs(gx0)-abs(gy0);
-	vec4 sz0=step(gz0,vec4(0.));
-	gx0-=sz0*(step(0.,gx0)-.5);
-	gy0-=sz0*(step(0.,gy0)-.5);
+	vec4 gx0 = ixy0 / 7.0;
+	vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+	gx0 = fract(gx0);
+	vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+	vec4 sz0 = step(gz0, vec4(0.0));
+	gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+	gy0 -= sz0 * (step(0.0, gy0) - 0.5);
 	
-	vec4 gx1=ixy1/7.;
-	vec4 gy1=fract(floor(gx1)/7.)-.5;
-	gx1=fract(gx1);
-	vec4 gz1=vec4(.5)-abs(gx1)-abs(gy1);
-	vec4 sz1=step(gz1,vec4(0.));
-	gx1-=sz1*(step(0.,gx1)-.5);
-	gy1-=sz1*(step(0.,gy1)-.5);
+	vec4 gx1 = ixy1 / 7.0;
+	vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+	gx1 = fract(gx1);
+	vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+	vec4 sz1 = step(gz1, vec4(0.0));
+	gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+	gy1 -= sz1 * (step(0.0, gy1) - 0.5);
 	
-	vec3 g000=vec3(gx0.x,gy0.x,gz0.x);
-	vec3 g100=vec3(gx0.y,gy0.y,gz0.y);
-	vec3 g010=vec3(gx0.z,gy0.z,gz0.z);
-	vec3 g110=vec3(gx0.w,gy0.w,gz0.w);
-	vec3 g001=vec3(gx1.x,gy1.x,gz1.x);
-	vec3 g101=vec3(gx1.y,gy1.y,gz1.y);
-	vec3 g011=vec3(gx1.z,gy1.z,gz1.z);
-	vec3 g111=vec3(gx1.w,gy1.w,gz1.w);
+	vec3 g000 = vec3(gx0.x, gy0.x, gz0.x);
+	vec3 g100 = vec3(gx0.y, gy0.y, gz0.y);
+	vec3 g010 = vec3(gx0.z, gy0.z, gz0.z);
+	vec3 g110 = vec3(gx0.w, gy0.w, gz0.w);
+	vec3 g001 = vec3(gx1.x, gy1.x, gz1.x);
+	vec3 g101 = vec3(gx1.y, gy1.y, gz1.y);
+	vec3 g011 = vec3(gx1.z, gy1.z, gz1.z);
+	vec3 g111 = vec3(gx1.w, gy1.w, gz1.w);
 	
-	vec4 norm0=taylorInvSqrt(vec4(dot(g000,g000),dot(g010,g010),dot(g100,g100),dot(g110,g110)));
-	g000*=norm0.x;
-	g010*=norm0.y;
-	g100*=norm0.z;
-	g110*=norm0.w;
-	vec4 norm1=taylorInvSqrt(vec4(dot(g001,g001),dot(g011,g011),dot(g101,g101),dot(g111,g111)));
-	g001*=norm1.x;
-	g011*=norm1.y;
-	g101*=norm1.z;
-	g111*=norm1.w;
+	vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+	g000 *= norm0.x;
+	g010 *= norm0.y;
+	g100 *= norm0.z;
+	g110 *= norm0.w;
+	vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+	g001 *= norm1.x;
+	g011 *= norm1.y;
+	g101 *= norm1.z;
+	g111 *= norm1.w;
 	
-	float n000=dot(g000,Pf0);
-	float n100=dot(g100,vec3(Pf1.x,Pf0.yz));
-	float n010=dot(g010,vec3(Pf0.x,Pf1.y,Pf0.z));
-	float n110=dot(g110,vec3(Pf1.xy,Pf0.z));
-	float n001=dot(g001,vec3(Pf0.xy,Pf1.z));
-	float n101=dot(g101,vec3(Pf1.x,Pf0.y,Pf1.z));
-	float n011=dot(g011,vec3(Pf0.x,Pf1.yz));
-	float n111=dot(g111,Pf1);
+	float n000 = dot(g000, Pf0);
+	float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+	float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+	float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+	float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+	float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+	float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+	float n111 = dot(g111, Pf1);
 	
-	vec3 fade_xyz=fade(Pf0);
-	vec4 n_z=mix(vec4(n000,n100,n010,n110),vec4(n001,n101,n011,n111),fade_xyz.z);
-	vec2 n_yz=mix(n_z.xy,n_z.zw,fade_xyz.y);
-	float n_xyz=mix(n_yz.x,n_yz.y,fade_xyz.x);
-	return 2.2*n_xyz;
+	vec3 fade_xyz = fade(Pf0);
+	vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+	vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+	float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+	return 2.2 * n_xyz;
 }
 
-float aastep(float threshold,float value){
+float aastep(float threshold, float value) {
 	#ifdef GL_OES_standard_derivatives
-	float afwidth=length(vec2(dFdx(value),dFdy(value)))*.70710678118654757;
-	return smoothstep(threshold-afwidth,threshold+afwidth,value);
+	float afwidth = length(vec2(dFdx(value), dFdy(value))) * 0.70710678118654757;
+	return smoothstep(threshold - afwidth, threshold + afwidth, value);
 	#else
-	return step(threshold,value);
+	return step(threshold, value);
 	#endif
 }
 
-float line(vec2 uv,float width){
-	float u=0.;
+float line(vec2 uv, float width) {
+	float u = 0.0;
 	
 	//try to hide some noise within the line: https://www.youtube.com/watch?v=2a2P8-PyNGA&list=PLswdBLT9llbheHhZdGNw9RehJP1kvpMHY&index=21&t=27m
-	float mn=.02;
-	if(uv.x<mn){
-		u=0.;
-	}else if(uv.x>1.-mn){
-		u=0.;
-	}else{
-		u=aastep(width,uv.x)-aastep(1.-width,uv.x);
+	float mn = 0.02;
+	if (uv.x < mn) {
+		u = 0.0;
+	}else if (uv.x > 1.0 - mn) {
+		u = 0.0;
+	}else {
+		u = aastep(width, uv.x) - aastep(1.0 - width, uv.x);
 	}
 	return u;
 }
 
-vec2 rotate(vec2 v,float a){
-	float s=sin(a);
-	float c=cos(a);
-	mat2 m=mat2(c,-s,s,c);
-	return m*v;
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, - s, s, c);
+	return m * v;
 }
 
-void main(){
+void main() {
 	
-	vec2 newUV=gl_FragCoord.xy/resolution.xy;
+	vec2 newUV = gl_FragCoord.xy / resolution.xy;
 	
-	newUV=rotate(newUV,rotation);
+	newUV = rotate(newUV, rotation);
 	
-	float noise=cnoise(vec3(newUV,time/10.));
+	float noise = cnoise(vec3(newUV, time / 10.0));
 	//repeat
-	newUV+=noise;
-	newUV=vec2(fract((newUV.x+newUV.y)*repeat),newUV.y);
+	newUV += noise;
+	newUV = vec2(fract((newUV.x + newUV.y) * repeat), newUV.y);
 	
 	// gl_FragColor=vec4(vec3(cnoise(vec4(vUv.x,vUv.y,time,1.))),1.);
 	
-	gl_FragColor=vec4(vec3(line(newUV.xy,lineWidth)),1.);
+	gl_FragColor = vec4(vec3(line(newUV.xy, lineWidth)), 1.0);
 	
 	// gl_FragColor=vec4(vUv,0.,1.);
 }
